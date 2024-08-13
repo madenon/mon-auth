8bc02b8c89af5ffd138d2ae90ccee3b9c22e1b66
madenon <brcgr230@gmail.com>
2024-08-13 20:27:16 +0100

    migration

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import { ClerkProvider,ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+import Loader from "@/components/Loader";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -10,10 +12,22 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
+
     <html lang="en">
       <body className={inter.className}>
+        <ClerkLoading>
+        <Loader/>
+
+        </ClerkLoading>
+        <ClerkLoaded>
         <Header/>
-        {children}</body>
+
+        {children}
+        </ClerkLoaded>
+       
+        </body>
     </html>
+    </ClerkProvider>
   );
 }
