6ce546e8bd129f9edbd8cd929298822704393ff0
madenon <brcgr230@gmail.com>
2024-08-13 18:56:45 +0100

    first commit

---
+++ src/app/layout.js
@@ -0,0 +1,19 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/Header";
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Next auth",
+  description: "Next auth with clerk and momgodb",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Header/>
+        {children}</body>
+    </html>
+  );
+}
